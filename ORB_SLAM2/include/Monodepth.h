11f91b6861e2b36b535b0d7e006b569aa8ae1057
yousibo <you18741856266@163.com>
2023-09-25 11:29:00 +0800

    融合monodepth进入orb2的cmakelists

---
+++ ORB_SLAM2/include/Monodepth.h
@@ -0,0 +1,28 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <iostream>
+#include <fstream>
+
+using namespace cv;
+using namespace dnn;
+using namespace std;
+
+class baseDepth
+{
+public:
+    baseDepth(int h, int w, const string &model_path = "model/mono.onnx")
+    {
+        this->inHeight = h;
+        this->inWidth = w;
+        cout << "start" << endl;
+        this->net = readNetFromONNX(model_path);
+        cout << "end" << endl;
+    };
+    Mat depth(Mat &frame);
+    Mat viewer(vector<Mat> imgs, double alpha = 0.80);
+
+private:
+    Net net;
+    int inWidth;
+    int inHeight;
+};
--- ORB_SLAM2/src/Monodepth.cc
+++ ORB_SLAM2/src/Monodepth.cc
@@ -2,30 +2,10 @@
 #include <opencv2/dnn.hpp>
 #include <iostream>
 #include <fstream>
-
+#include "Monodepth.h"
 using namespace cv;
 using namespace dnn;
 using namespace std;
-
-class baseDepth
-{
-public:
-    baseDepth(int h, int w, const string &model_path = "model/mono.onnx")
-    {
-        this->inHeight = h;
-        this->inWidth = w;
-        cout << "start" << endl;
-        this->net = readNetFromONNX(model_path);
-        cout << "end" << endl;
-    };
-    Mat depth(Mat &frame);
-    Mat viewer(vector<Mat> imgs, double alpha = 0.80);
-
-private:
-    Net net;
-    int inWidth;
-    int inHeight;
-};
 
 Mat baseDepth::depth(Mat &frame)
 {
@@ -63,42 +43,4 @@ Mat baseDepth::viewer(vector<Mat> imgs, double alpha)
     resize(imgs[1], imgStd, imgStdSize, alpha, alpha, INTER_LINEAR);
     imgStd.copyTo(imgWindow(Rect(Point2i(imgStdSize.width + 2 * delta_w, delta_h), imgStdSize)));
     return imgWindow;
-}
-
-int main(int argc, char **argv)
-{
-    if (argc < 3)
-    {
-        std::cerr << "Usage: " << argv[0] << " <video_file_path> <model_path>" << std::endl;
-        return 1;
-    }
-
-    VideoCapture capture(0); // 打开视频文件
-    if (!capture.isOpened())
-    {
-        std::cerr << "Error: Could not open video file." << std::endl;
-        return 1;
-    }
-
-    int h = 192, w = 640;
-    baseDepth model(h, w, argv[2]);
-
-    static const std::string kWinName = "Deep learning Mono depth estimation in OpenCV";
-    namedWindow(kWinName, WINDOW_NORMAL);
-
-    Mat frame;
-    while (capture.read(frame)) // 从视频中读取每一帧
-    {
-        Mat depthMap = model.depth(frame);
-
-        Mat res = model.viewer({frame, depthMap}, 0.90);
-        imshow(kWinName, res);
-
-        char key = waitKey(1);
-        if (key == 27) // 按下ESC键退出循环
-            break;
-    }
-
-    destroyAllWindows();
-    return 0;
 }
